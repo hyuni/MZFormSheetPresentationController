bb5c609f65b3dee502eb0af6749658c929e90dc5
hyuni <hyuni@hyuniui-MacBook-Pro.local>
2017-09-26 01:17:13 +0900

    test

--- Example/Swift/MZFormSheetPresentationController Swift Example/CustomTransition.swift
+++ Example/Swift/MZFormSheetPresentationController Swift Example/CustomTransition.swift
@@ -7,32 +7,33 @@
 //
 
 import UIKit
+import MZFormSheetPresentationController
 
 class CustomTransition: NSObject, MZFormSheetPresentationViewControllerTransitionProtocol {
    
-    func entryFormSheetControllerTransition(formSheetController: UIViewController, completionHandler: MZTransitionCompletionHandler) {
+    func entryFormSheetControllerTransition(_ formSheetController: UIViewController, completionHandler:  @escaping MZTransitionCompletionHandler) {
         let bounceAnimation = CAKeyframeAnimation(keyPath: "transform")
         bounceAnimation.fillMode = kCAFillModeBoth
-        bounceAnimation.removedOnCompletion = true
+        bounceAnimation.isRemovedOnCompletion = true
         bounceAnimation.duration = 0.4
         bounceAnimation.values = [
-            NSValue(CATransform3D: CATransform3DMakeScale(0.01, 0.01, 0.01)),
-            NSValue(CATransform3D: CATransform3DMakeScale(0.9, 0.9, 0.9)),
-            NSValue(CATransform3D: CATransform3DMakeScale(1.1, 1.1, 1.1)),
-            NSValue(CATransform3D: CATransform3DIdentity)
+            NSValue(caTransform3D: CATransform3DMakeScale(0.01, 0.01, 0.01)),
+            NSValue(caTransform3D: CATransform3DMakeScale(0.9, 0.9, 0.9)),
+            NSValue(caTransform3D: CATransform3DMakeScale(1.1, 1.1, 1.1)),
+            NSValue(caTransform3D: CATransform3DIdentity)
         ]
         bounceAnimation.keyTimes = [0.0, 0.5, 0.75, 1.0]
         bounceAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut),CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut),CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-        bounceAnimation.delegate = self
+        bounceAnimation.delegate = self as! CAAnimationDelegate
         bounceAnimation.setValue((completionHandler as? AnyObject)!, forKey: "completionHandler")
-        formSheetController.view.layer.addAnimation(bounceAnimation, forKey: "bounce")
+        formSheetController.view.layer.add(bounceAnimation, forKey: "bounce")
     }
     
-    func exitFormSheetControllerTransition(formSheetController: UIViewController, completionHandler: MZTransitionCompletionHandler) {
+    func exitFormSheetControllerTransition(_ formSheetController: UIViewController, completionHandler: @escaping MZTransitionCompletionHandler) {
         var formSheetRect = formSheetController.view.frame
-        formSheetRect.origin.x = UIScreen.mainScreen().bounds.width
-        
-        UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+        formSheetRect.origin.x = UIScreen.main.bounds.width
+		
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
             formSheetController.view.frame = formSheetRect
         }, completion: {(value: Bool)  -> Void in
             completionHandler()
